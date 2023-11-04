f731c6badeacc56fb49e5b505302b081a7ae3784
dolly0920 <dolly0920@naver.com>
2023-11-04 14:25:45 +0900

    feat : t-ming 프로젝트 스프링 부트 초기 세팅
    - java 17, spring boot 3.1.5 버전 셋팅

---
+++ src/main/java/com/study/toyproject/tming/TMingApplication.java
@@ -0,0 +1,13 @@
+package com.study.toyproject.tming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TMingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/toyproject/tming/TMingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.toyproject.tming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TMingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
