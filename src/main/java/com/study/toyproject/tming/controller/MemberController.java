7e7c0db4c7b1eb8ce3d9b58137f2e446ed3d93b9
ohj72123@gmail.com <ohj72123@gmail.com>
2023-12-20 21:14:09 +0900

    feat: 타임리프 초기 세팅 설정 및 데이터베이스 정보 추가
    - 타임리프 레이아웃, 공통 레이아웃 정의
    - jquery 추가
    - member 컨트롤러 추가
    - 타임리프 의존성, 라이브러리 추가
    - 데이터베이스 url, username, password 추가

---
+++ src/main/java/com/study/toyproject/tming/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.study.toyproject.tming.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(value = "/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+	@GetMapping("/login")
+	public String login () {
+
+		return "member/login";
+	}
+}
