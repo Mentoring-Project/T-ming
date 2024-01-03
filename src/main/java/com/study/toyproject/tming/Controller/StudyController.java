15fcb7ca32ddce8573aa0ddaf078f2c38fab1127
AYeong-Jeon <102805138+AYeong-Jeon@users.noreply.github.com>
2024-01-03 21:05:54 +0900

    Feat : 페이지 연결 및 화면 작업 1차

--- src/main/java/com/study/toyproject/tming/Controller/StudyController.java
+++ src/main/java/com/study/toyproject/tming/Controller/StudyController.java
@@ -3,6 +3,7 @@ package com.study.toyproject.tming.Controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StudyController {
 
     @GetMapping("/list")
-    public String StudyList () {
+    public String StudyList (Model model) {
 
+        model.addAttribute("list", null);
 
-        return "/study/studyList";
+        return "/studyList";
     }
 
 
