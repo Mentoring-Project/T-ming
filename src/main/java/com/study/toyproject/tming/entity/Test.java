90858d5469ba448c24f56c92f0bbc2f27101699a
dolly0920 <dolly0920@naver.com>
2024-02-16 22:05:47 +0900

    test: 커밋 1

---
+++ src/main/java/com/study/toyproject/tming/entity/Test.java
@@ -0,0 +1,13 @@
+package com.study.toyproject.tming.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Test {
+
+    private int number;
+    private String name;
+    private int fixup;
+}
